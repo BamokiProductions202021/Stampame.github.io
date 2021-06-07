7e903a5e8661a136745bad0d2ce9e36d77464eda
Stampame <74021393+BamokiProductions202021@users.noreply.github.com>
2021-06-07 19:05:56 -0400

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,10 @@
+let nombre = prompt("dime tu nombre?");
+ console.log("hola wey")
+  console.info("hola wey") 
+  console.warn("hola wey")
+   console.error("hola wey")
+    console.debug("hola wey")
+   
+ 
+ alert("hola " +nombre+ " y bienvenido a la galeria virtual de Stampame")
+ 
